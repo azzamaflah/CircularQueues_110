1b5c42c564b576b69b44ca6e794113954781aea0
azzamaflah <azzamhebat46@gmail.com>
2023-06-22 15:19:08 +0700

    untuk mengecek jika antrian hanya memiliki satu elemen

--- displayQueues/displayQueues.cpp
+++ displayQueues/displayQueues.cpp
@@ -46,5 +46,13 @@ public:
 			return;
 		}
 		cout << "\nThe Element deleted rom the queue is: " << queue_array[FRONT] << "\n";
-	}
+
+		// cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			
+
 };
